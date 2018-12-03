b00e88116c06292753eacef7e6a18f5658551653
Solon Oliveira <solon.neto@eic.cefet-rj.br>
2018-12-03 00:01:44 -0200

    Adicionado método para recuperar as questões de um usuário

--- quizzing/src/main/java/br/com/cefetrj/ws/quizzing/repository/QuestionRepository.java
+++ quizzing/src/main/java/br/com/cefetrj/ws/quizzing/repository/QuestionRepository.java
@@ -2,9 +2,15 @@ package br.com.cefetrj.ws.quizzing.repository;
 
 import br.com.cefetrj.ws.quizzing.model.question.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface QuestionRepository extends JpaRepository<Question, Long>
 {
+	@Query("select q from questions where q.user_id = :userId")
+	List<Question> findAllUserQuestions(@Param("userId") Long userId);
 }
